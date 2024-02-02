967705ad8c85336294589408f996e3f68bdca108
e4d08 <dythe.x@mail.ru>
2024-02-02 23:23:13 +0300

    logger.ts: add comments

--- src/logger.ts
+++ src/logger.ts
@@ -1,21 +1,41 @@
 import chalk from "chalk";
 
+/**
+ * Writes string to stdout
+ * @param str string to write
+ */
 export function writeline(str: string) {
 	process.stdout.write(`${str}\n`);
 }
 
+/**
+ * Writes message as log
+ * @param str message to write
+ */
 export function log(str: string) {
 	writeline(chalk.greenBright(`[LOG] ${str}`));
 }
 
+/**
+ * Writes message as error to stderr
+ * @param str message to write
+ */
 export function error(str: string) {
 	process.stderr.write(chalk.redBright(`[ERROR] ${str}\n`));
 }
 
+/**
+ * Writes message as warning
+ * @param str message to write
+ */
 export function warning(str: string) {
 	writeline(chalk.yellowBright(`[WARNING] ${str}`));
 }
 
+/**
+ * Writes message as info
+ * @param str message to write
+ */
 export function info(str: string) {
 	writeline(chalk.blueBright(`[INFO] ${str}`));
 }
