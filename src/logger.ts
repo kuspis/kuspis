7182b8c24581ef242df5e0dceea92ac27cf16dd0
e4d08 <dythe.x@mail.ru>
2024-02-02 21:13:40 +0300

    Refactor logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,23 +1,21 @@
 import chalk from "chalk";
 
-export default class Logger {
-	public writeline(str: string) {
-		process.stdout.write(`${str}\n`);
-	}
+export function writeline(str: string) {
+	process.stdout.write(`${str}\n`);
+}
 
-	public log(str: string) {
-		this.writeline(chalk.greenBright(`[LOG] ${str}`));
-	}
+export function log(str: string) {
+	writeline(chalk.greenBright(`[LOG] ${str}`));
+}
 
-	public error(str: string) {
-		process.stderr.write(chalk.redBright(`[ERROR] ${str}\n`));
-	}
+export function error(str: string) {
+	process.stderr.write(chalk.redBright(`[ERROR] ${str}\n`));
+}
 
-	public warning(str: string) {
-		this.writeline(chalk.yellowBright(`[WARNING] ${str}`));
-	}
+export function warning(str: string) {
+	writeline(chalk.yellowBright(`[WARNING] ${str}`));
+}
 
-	public info(str: string) {
-		this.writeline(chalk.blueBright(`[INFO] ${str}`));
-	}
+export function info(str: string) {
+	writeline(chalk.blueBright(`[INFO] ${str}`));
 }
